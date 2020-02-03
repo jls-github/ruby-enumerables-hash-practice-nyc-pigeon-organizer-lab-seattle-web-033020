e34af8e940cccb415f061a3b4e0503b85b850ed0
John Souza <souzajohnlee@gmail.com>
2020-02-03 23:23:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  #create a new array of hashes
+  #iterate over each array list of names (hash, hash, hash, array)
+  #Make that name a new directory in the hash list OR update the hash list with the new data
+  #Setup will be -- 
+  #[Array name => h2 hash => h1
+  #The h2 key will become the value, the h1 key will become the key
+  
+  result = {}
+  data.each do |category, category_values|
+    specific_category = category
+    category_values.each do |characteristic, characteristic_values|
+      specific_characteristic = characteristic
+      characteristic_values.each do |name|
+        if result.include?(name)
+          if result[name].include?(specific_category)
+            result[name][specific_category].push(specific_characteristic.to_s)
+          else
+            result[name][specific_category] = [specific_characteristic.to_s]
+          end
+        else
+          result[name] = {specific_category => [specific_characteristic.to_s]}
+        end
+      end
+    end
+  end
+  return result
+  
+  
 end
